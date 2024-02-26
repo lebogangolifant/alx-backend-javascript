c6746bf95be708ebe5da334bc16a94516989d4e3
Lebogang <lebogangodeveloper@gmail.com>
2024-02-26 12:08:19 +0000

    Write the routes

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const router = express.Router();
+
+// Route for homepage
+router.get('/', AppController.getHomepage);
+
+// Routes for students
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+export default router;
