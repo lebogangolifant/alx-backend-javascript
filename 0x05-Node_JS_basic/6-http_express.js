39fa2295ec1eee2bf9147e3766736938dae65ca9
Lebogang <lebogangodeveloper@gmail.com>
2024-02-26 11:11:19 +0000

    Create HTTP server using Express module

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Create Express application
+const app = express();
+
+// Define route for the '/' endpoint
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!\n');
+});
+
+// Start server on port 1245
+app.listen(1245, () => {
+    console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
