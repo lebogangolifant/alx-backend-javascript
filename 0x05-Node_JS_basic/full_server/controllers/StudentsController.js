5628fa5f08a7056b9dd2f025ad0bb2e174dfd2eb
Lebogang <lebogangodeveloper@gmail.com>
2024-02-26 11:59:22 +0000

    Write the Students controller
    - Implement StudentsController class with getAllStudents and getAllStudentsByMajor methods
    - 'getAllStudents' method accepts request and response objects as arguments, returns a status 200,
    and displays a list of students grouped by field from the database.
    - 'getAllStudentsByMajor' method accepts request and response objects as arguments,
    validates the major parameter, and returns a list of students for the specified major from the database.

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,49 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const studentsByField = await readDatabase(process.argv[2]);
+      const fields = Object.keys(studentsByField).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+
+      const response = ['This is the list of our students'];
+
+      fields.forEach(field => {
+        const students = studentsByField[field];
+        const count = students.length;
+        const firstNames = students.join(', ');
+        response.push(`Number of students in ${field}: ${count}. List: ${firstNames}`);
+      });
+
+      res.status(200).send(response.join('\n'));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    try {
+      const studentsByField = await readDatabase(process.argv[2]);
+      const major = req.query.major.toUpperCase();
+
+      if (major !== 'CS' && major !== 'SWE') {
+        return res.status(500).send('Major parameter must be CS or SWE');
+      }
+
+      if (!studentsByField[major]) {
+        return res.status(200).send(`No students found for major ${major}`);
+      }
+
+      const students = studentsByField[major];
+      const firstNames = students.join(', ');
+
+      res.status(200).send(`List: ${firstNames}`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
