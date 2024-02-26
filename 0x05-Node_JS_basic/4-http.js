bb8920968a5b2eeff5ec4c5572936f75598dfc8f
Lebogang <lebogangodeveloper@gmail.com>
2024-02-26 10:15:49 +0000

    Create HTTP server using http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+// Create HTTP server
+const app = http.createServer((req, res) => {
+    // Set response header
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send response body
+    res.end('Hello Holberton School!');
+});
+
+// Listen on port 1245
+app.listen(1245, () => {
+    console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
