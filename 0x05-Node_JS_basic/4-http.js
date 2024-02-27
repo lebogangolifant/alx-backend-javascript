3e6ded9cf9eb7a9f472939c67d0e9f8196a529ee
Lebogang <lebogangodeveloper@gmail.com>
2024-02-27 09:08:39 +0000

    Fix object-curly-spacing and indentation

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,16 +2,16 @@ const http = require('http');
 
 // Create HTTP server
 const app = http.createServer((req, res) => {
-    // Set response header
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Set response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // Send response body
-    res.end('Hello Holberton School!');
+  // Send response body
+  res.end('Hello Holberton School!');
 });
 
 // Listen on port 1245
 app.listen(1245, () => {
-    console.log('Server running at http://localhost:1245/');
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
