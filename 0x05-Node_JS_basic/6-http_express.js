0a50b7266c1e8305454e63a99b6964f23092c414
Lebogang <lebogangodeveloper@gmail.com>
2024-02-27 09:19:39 +0000

    Fix indentation error

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -5,12 +5,12 @@ const app = express();
 
 // Define route for the '/' endpoint
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!\n');
 });
 
 // Start server on port 1245
 app.listen(1245, () => {
-    console.log('Server running at http://localhost:1245/');
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
