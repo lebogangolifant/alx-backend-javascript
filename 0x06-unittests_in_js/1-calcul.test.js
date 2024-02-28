b476c5338d9a79484eb108c53a8abe79552f6937
Lebogang <lebogangodeveloper@gmail.com>
2024-02-28 10:56:47 +0000

    Add upgraded calculateNumber function
    and corresponding tests

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('should return the rounded sum of two numbers', () => {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+    });
+
+    describe('SUBTRACT', () => {
+        it('should return the rounded difference of two numbers', () => {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+    });
+
+    describe('DIVIDE', () => {
+        it('should return the rounded division result of two numbers', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+
+        it('should return "Error" if the second number is rounded to 0', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+    });
+});
