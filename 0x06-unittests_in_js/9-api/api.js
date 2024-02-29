c8e815874ba44983c1d2c452d7d219ed486431dc
Lebogang <lebogangodeveloper@gmail.com>
2024-02-29 10:34:41 +0000

    Add endpoint for cart with ID validation and corresponding tests

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.param('id', (req, res, next, id) => {
+  if (!/^\d+$/.test(id)) {
+    return res.status(400).send('Invalid ID. ID must be a number.');
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = server;
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Cart page', () => {
+  let server;
+
+  before((done) => {
+    server = require('./api');
+    done();
+  });
+
+  after((done) => {
+    server.close(() => {
+      done();
+    });
+  });
+
+  it('Correct status code when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct status code when :id is NOT a number (=> 404)', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  };
+});
+
