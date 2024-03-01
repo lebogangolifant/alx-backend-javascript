e1aa9314d35731ab4e63a0841ca41f1a15571cd5
Lebogang <lebogangodeveloper@gmail.com>
2024-03-01 10:33:53 +0000

    Update test suite for second number rounded

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,9 +11,10 @@ describe('calculateNumber', () => {
     });
 
     it('should return the rounded sum when the second number is rounded', () => {
-        assert.strictEqual(calculateNumber(1.3, 3), 4);
-        assert.strictEqual(calculateNumber(1.7, 3), 5);
-        assert.strictEqual(calculateNumber(1.2, 3), 4);
-        assert.strictEqual(calculateNumber(1.5, 3), 5);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1.6, 3), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.8), 5);
+        assert.strictEqual(calculateNumber(-1, -3), -4);
+        assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
     });
 });
