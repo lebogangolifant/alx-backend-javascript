35bea5ede99395a0904d2c07540e0b640300518c
Lebogang <lebogangodeveloper@gmail.com>
2024-03-01 09:43:13 +0000

    Add test suite for calculateNumber function
    - for the second number rounded

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,4 +9,11 @@ describe('calculateNumber', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+
+    it('should return the rounded sum when the second number is rounded', () => {
+        assert.strictEqual(calculateNumber(1.3, 3), 4);
+        assert.strictEqual(calculateNumber(1.7, 3), 5);
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
+        assert.strictEqual(calculateNumber(1.5, 3), 5);
+    });
 });
